f8fd7be1df75a489c797ba7964a66362e7d76c4a
joseangelrg1224 <115759500+joseangelrg1224@users.noreply.github.com>
2023-04-05 19:25:52 -0500

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -3,6 +3,14 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    firstname: {
+        type: 'string',
+        default: ''
+    },
+    lastname: {
+        type: 'string',
+        default: ''
+    },
     admin: {
         type: Boolean,
         default: false
